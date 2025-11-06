7e6d8fa1033bb3376e78e6b0172176ed24eb8494
Chioma533 <queenmichael527@gmail.com>
2025-11-06 15:53:04 +0100

    completed boilerplate

---
+++ src/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "./components/ui/button"
+const Routes = () => {
+  return (
+    <div className='bg-green-300'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Routes
