0d73cdb70757d6ec7bfb7198e645211ca27013a8
Levi ikechukwu <leviikechukwu12@gmail.com>
2025-11-27 06:48:24 +0100

    Merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Home from "./pages/landingPage/Home";
 import Menu from "./pages/menuPage/Menu";
@@ -7,16 +6,8 @@ import ProductDetails from "./pages/product details/ProductDetails";
 import Cart from "./pages/cartPage/Cart";
 import { CartProvider } from "./components/shared/CartProvider";
 import AboutUs from "./pages/about-us/AboutUs";
-=======
-import React from 'react'
-import Home from './pages/landingPage/Home'
-import Menu from './pages/menuPage/Menu'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ProductDetails from './pages/product details/ProductDetails'
-import Cart from './pages/cartPage/Cart'
-import { CartProvider } from './components/shared/CartProvider'
-import Checkout from './pages/checkoutPage/Checkout'
->>>>>>> 0285f0cd7d2d1619f340caa1d94dde52d7199342
+import Checkout from "./pages/checkoutPage/Checkout";
+
 const App = () => {
   return (
     <BrowserRouter>
@@ -25,18 +16,11 @@ const App = () => {
           {/* dynamic product route */}
           <Route path="/product/:id" element={<ProductDetails />} />
           {/* other routes */}
-<<<<<<< HEAD
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/about" element={<AboutUs />} />
-=======
-          <Route path='/' element={<Home />} />
-          <Route path='/menu' element={<Menu />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/checkout' element={<Checkout />} />
-
->>>>>>> 0285f0cd7d2d1619f340caa1d94dde52d7199342
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </CartProvider>
     </BrowserRouter>
