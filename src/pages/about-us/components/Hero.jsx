dad1fb3fe5dcc9534657e64f4daeeeb2070042af
Levi ikechukwu <leviikechukwu12@gmail.com>
2025-11-28 10:46:14 +0100

    Changes

--- src/pages/about-us/components/Hero.jsx
+++ src/pages/about-us/components/Hero.jsx
@@ -1,11 +1,28 @@
 import React from "react";
-import img1 from "@/assets/Abt1.svg";
+import Event1 from "@/assets/Abt1.jpg";
 const Hero = () => {
   return (
-    <section className="mt-16">
-      <div>
-        <img src={img1} alt="" />
+    <section className="h-[550px] relative">
+      <div className="w-full absolute">
+        <img
+          src={Event1}
+          className="h-[550px] relative z-1 object-cover w-full rounded-b-[10px]"
+          alt=""
+        />
+        <div className="bg-black/50 w-full h-[550px] absolute top-0 z-2 rounded-b-[50px]"></div>
       </div>
+
+      <div className="absolute pl-[150px] w-[80%] top-[150px] z-3">
+        <h1 className="text-5xl leading-[1.2] pb-3 text-white font-bold">
+          Catering for Every Event — Fresh, Hygienic, and Delicious
+        </h1>
+        <p className="text-white w-[70%] text-lg">
+          From intimate gatherings to large celebrations, we prepare authentic
+          Nigerian dishes that bring people together — cooked fresh, beautifully
+          packaged, and handled with strict hygiene standards.
+        </p>
+      </div>
+      <div></div>
     </section>
   );
 };
